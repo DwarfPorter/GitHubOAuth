561399f66446a86635954b506be53050bbb167c0
DwarfPorter <mrvlmor@gmail.com>
2019-01-12 00:05:15 +0300

    oauth for github

---
+++ GitHubOAuth/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubOAuth
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
